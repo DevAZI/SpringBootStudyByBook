9f6f77bf063d4c0884e457e9e2525b23cf4f8a39
DevAZI <80841284+DevAZI@users.noreply.github.com>
2021-09-20 19:39:16 +0900

    글삭제

--- src/main/java/com/j/SpringBootStudy/web/PostsApiController.java
+++ src/main/java/com/j/SpringBootStudy/web/PostsApiController.java
@@ -19,6 +19,11 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
